a03029b1a50e9cf56c3167f12c5e5847e92a944e
LinconOssa <lossa@sistemasenlinea.com.co>
2025-04-21 20:46:27 -0500

    fix: refactor requestProcessModal

--- src/design/modals/requestProcessModal/index.tsx
+++ src/design/modals/requestProcessModal/index.tsx
@@ -1,33 +1,15 @@
 import { createPortal } from "react-dom";
-import { RequestStatus } from "@design/feedback/requestStatus";
 import { basic } from "@design/tokens";
-import {
-  Blanket,
-  Text,
-  Stack,
-  Spinner,
-  useMediaQuery,
-} from "@inubekit/inubekit";
-import { ComponentAppearance } from "@ptypes/aparences.types";
-import { RequestProcess } from "@design/feedback/requestProcess";
+import { Blanket, Stack, useMediaQuery } from "@inubekit/inubekit";
 import { IRequestProcessModal } from "@ptypes/IRequestProcessModal";
-import { statusFlowAutomatic } from "@config/status/statusFlowAutomatic";
 import { StyledModal } from "./styles";
+import { ContentHandler } from "./contentHandler";
+import { LoadingContent } from "./loadingContent";
 
 const RequestProcessModal = (props: IRequestProcessModal) => {
-  const {
-    descriptionRequestProcess,
-    portalId,
-    requestProcessSteps,
-    loading,
-    appearance = "primary",
-    saveData,
-    descriptionRequestStatus,
-    onCloseRequestStatus,
-  } = props;
+  const { portalId, loading, ...restProps } = props;
 
   const isMobile = useMediaQuery("(max-width: 768px)");
-
   const node = document.getElementById(portalId);
 
   if (!node) {
@@ -41,63 +23,9 @@ const RequestProcessModal = (props: IRequestProcessModal) => {
       <StyledModal $smallScreen={isMobile}>
         <Stack direction="column" gap={basic.spacing.s300}>
           {loading ? (
-            <Stack
-              direction="column"
-              alignItems="center"
-              justifyContent="center"
-              alignContent="center"
-              gap={basic.spacing.s300}
-            >
-              <Spinner
-                size="large"
-                appearance={appearance}
-                transparent={false}
-              />
-              <Text type="body" size="medium" weight="bold" appearance="dark">
-                Espere un momento por favor
-              </Text>
-            </Stack>
+            <LoadingContent />
           ) : (
-            saveData &&
-            saveData.requestStatus !== "" &&
-            saveData.requestNumber !== "" &&
-            (statusFlowAutomatic.includes(saveData.requestStatus) ? (
-              <RequestProcess
-                title={descriptionRequestProcess.title}
-                description={descriptionRequestProcess.description}
-                appearance={ComponentAppearance.SUCCESS}
-                requestSteps={requestProcessSteps}
-                isMobile={isMobile}
-                sizeIcon={isMobile ? "20px " : "32px"}
-              />
-            ) : (
-              <RequestStatus
-                portalId="portal"
-                title={
-                  descriptionRequestStatus(
-                    saveData.requestNumber,
-                    saveData.settingRequestId
-                  ).title
-                }
-                requestNumber={saveData.requestNumber}
-                description={
-                  descriptionRequestStatus(
-                    saveData.requestNumber,
-                    saveData.settingRequestId
-                  ).description
-                }
-                onClick={onCloseRequestStatus}
-                onCloseModal={onCloseRequestStatus}
-                isLoading={false}
-                actionText={
-                  descriptionRequestStatus(
-                    saveData.requestNumber,
-                    saveData.settingRequestId
-                  ).actionText
-                }
-                appearance={ComponentAppearance.PRIMARY}
-              />
-            ))
+            <ContentHandler {...restProps} isMobile={isMobile} />
           )}
         </Stack>
       </StyledModal>

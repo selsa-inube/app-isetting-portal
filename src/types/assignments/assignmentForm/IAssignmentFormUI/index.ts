afd7cbc8b128414f7aabc2e3c82077c5250344fd
heber59 <hesan0308@gmail.com>
2025-12-09 17:30:38 -0500

    fix: adjust imports

--- src/types/assignments/assignmentForm/IAssignmentFormUI/index.ts
+++ src/types/assignments/assignmentForm/IAssignmentFormUI/index.ts
@@ -1,7 +1,7 @@
 import { IFormEntry } from "../IFormEntry";
 import { IMenuOptions } from "@ptypes/design/IMenuOptions";
 import { IOption } from "@inubekit/inubekit";
-import { IToggleTableColumn } from "@design/forms/assignmentForm/tableGroup";
+import { IToggleTableColumn } from "../tableGroup/IToggleTableColumn";
 
 interface IAssignmentFormUI {
   filteredEntries: IFormEntry[];

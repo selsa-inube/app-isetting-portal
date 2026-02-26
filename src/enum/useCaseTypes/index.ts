89c5fa0beb08a35ccb17799899139149c020113a
heber59 <hesan0308@gmail.com>
2026-02-26 12:25:35 -0500

    fix: adjust use cases enum

--- src/enum/useCaseTypes/index.ts
+++ src/enum/useCaseTypes/index.ts
@@ -6,12 +6,12 @@ enum EUseCaseTypes {
   EDIT_USER = "Modificar funcionario",
   DELETE_USER = "Eliminar funcionario",
 
-  ADD_MISSION = "Agregar cargo para la unidad de negocio",
-  EDIT_MISSION = "Modificar cargo de la unidad de negocio",
-  DELETE_MISSION = "Eliminar cargos de la unidad de negocio",
+  ADD_MISSION = "Agregar cargo para funcionarios del operador",
+  EDIT_MISSION = "Modificar cargo del operador",
+  DELETE_MISSION = "Eliminar cargos del operador",
 
-  ADD_POSITION = "Agregar cargo para funcionarios del operador",
-  EDIT_POSITION = "Modificar cargo del operador",
-  DELETE_POSITION = "Eliminar cargos del operador",
+  ADD_POSITION = "Agregar cargo para la unidad de negocio",
+  EDIT_POSITION = "Modificar cargo de la unidad de negocio",
+  DELETE_POSITION = "Eliminar cargos de la unidad de negocio",
 }
 export { EUseCaseTypes };
